941a5c0fa29eb0c35c26ebcd503ab05a04e8b3b8
sxrxhjxckson <sarah.anne.jackson.24@gmail.com>
2026-07-08 14:04:39 -0400

    Update database_insertion.py
    Added another patient and professional to test if professional can only see their own patients

--- database_insertion.py
+++ database_insertion.py
@@ -4,21 +4,32 @@ from DatabaseManager import DatabaseManager
 
 dbm = DatabaseManager("care_path_db.db")
 
-patient_id = dbm.add_user("test_patient@test.com","password","Patient")
-prof_id = dbm.add_user("test_professional@test.com","password","Professional")
+patient_id_one = dbm.add_user("test_patient@test.com","password","Patient")
+prof_id_one = dbm.add_user("test_professional@test.com","password","Professional")
+patient_id_two = dbm.add_user("test_patient_two@test.com","password","Patient")
+prof_id_two = dbm.add_user("test_professional_two@test.com","password","Professional")
 
-dbm.add_patient(patient_id, "John","Doe","01/01/2000",123,456)
+dbm.add_patient(patient_id_one, "John","Doe","01/01/2000",123,456)
 
-dbm.add_professional(prof_id, "Gregory", "House", "6767")
+dbm.add_professional(prof_id_one, "Gregory", "House", "6767")
 
+dbm.add_patient(patient_id_two, "Jane","Doe","10/01/2005",456,123)
+
+dbm.add_professional(prof_id_two, "Kyle", "Smith", "6969")
 
 dbm.add_medication("Pills", "The good stuff", "Once daily")
 
-dbm.add_prescription(1,2,1,"test dosage", "26/06/2026","26/07/2026",1)
+dbm.add_prescription(patient_id_one,prof_id_one,1,"test dosage", "26/06/2026","26/07/2026",1)
 
-dbm.add_test_result(1,2,"Test test results", "26/06/2026","Pending","These are the comments")
+dbm.add_test_result(patient_id_one,prof_id_one,"Test test results", "26/06/2026","Pending","These are the comments")
 
-dbm.add_appointement(1,2,"26/06/2026","14:05","Confirmed", "These are the notes")
+dbm.add_appointement(patient_id_one,prof_id_one,"26/06/2026","14:05","Confirmed", "These are the notes")
+
+dbm.add_prescription(patient_id_two,prof_id_two,1,"50mg", "28/06/2026","28/07/2026",1)
+
+dbm.add_test_result(patient_id_two,prof_id_two,"Test results", "28/06/2026","Pending","These are the comments")
+
+dbm.add_appointement(patient_id_two,prof_id_two,"28/06/2026","10:0","Confirmed", "These are the notes")
 
 dbm.add_message(1,2,"2024-05-08 14:35:29.123", "This is a test message")
 
